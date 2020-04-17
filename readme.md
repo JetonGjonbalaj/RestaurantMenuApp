4d6624a1a52e19ec32396fbd6bd3e7b419d50b7b
Jeton Gjonbalaj <jetonmgjonbalaj@gmail.com>
2020-04-17 17:38:43 +0200

    now you can see restaurants, create a new one or edit one

--- readme.md
+++ readme.md
@@ -1,2 +1,11 @@
 This project is something small and lightweight done with Flask.
 I am doing this to practice my backend skills with python.
+
+#if you want to try it to your computer
+
+1. You need python v3.x to run this code
+2. With cmd navigate to the folder where you extracted it
+3. Run the command 'python3 database_setup.py'
+4. Now run the command 'python3 server.py'
+
+Enjoy
--- server.py
+++ server.py
@@ -1,27 +1,56 @@
-from flask import Flask
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# import cgi
+from flask import Flask, render_template, url_for, request, redirect
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+import cgi
 
 app = Flask(__name__)
 
-# engine = create_engine('sqlite:///database.db')
-# Base.metadata.bind = engine
-# DBSession = sessionmaker(bind = engine)
-# session = DBSession()
+engine = create_engine('sqlite:///database.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/restaurants')
-def ShowRestaurants():
-    return 'This page will show all the restaurants'
+def showRestaurants():
+    restaurant_list = session.query(Restaurant).all()
+    return render_template('restaurants.html', restaurants = restaurant_list)
 
-@app.route('/restaurants/new')
+@app.route('/restaurants/new', methods = ['GET', 'POST'])
 def newRestaurant():
-    return 'This page will be for making a new restaurant'
+    if request.method == 'POST':
+        restaurant_name = request.form['name']
+
+        if (restaurant_name.strip() == ''):
+            return redirect(url_for('newRestaurant'))
+        else:
+            new_restaurant = Restaurant(name = restaurant_name)
+            session.add(new_restaurant)
+            session.commit()
+            return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('newRestaurant.html')
 
 @app.route('/restaurants/<int:restaurant_id>/edit', methods = ['GET', 'POST'])
 def editRestaurant(restaurant_id):
-    return 'This page will be for editing %s' % restaurant_id
+    if request.method == 'POST':
+        edit_restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one_or_none()
+        if edit_restaurant == None:
+            return redirect(url_for('showRestaurants'))
+        else:
+            restaurant_name = request.form['name']
+
+            edit_restaurant.name = restaurant_name
+            session.add(edit_restaurant)
+            session.commit()
+            return redirect(url_for('showRestaurants'))
+    else:
+        edit_restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one_or_none()
+        if edit_restaurant == None:
+            return redirect(url_for('showRestaurants'))
+        else:
+            return render_template('editRestaurant.html', restaurant = edit_restaurant)
 
 @app.route('/restaurants/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
